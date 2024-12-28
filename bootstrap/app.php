212a0cb48342e05b8946a36e2a220e5694f3fea6
Tayo yewon <temit4ever@yahoo.com>
2024-12-28 08:50:23 +0000

    add auth middleware to api route

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,17 @@
 <?php
 
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Auth\Middleware\AuthenticateWithBasicAuth;
+use Illuminate\Auth\Middleware\Authenticate;
+
+use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,7 +21,20 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+
+        // This setup below allows the routes in the api file to use authentication (logged-in user)
+        // Only authenticated user can have access to the routes
+        $middleware->group('api', [
+           // ValidateCsrfToken::class,
+            //'throttle:api',
+            //SubstituteBindings::class,
+            StartSession::class,
+            EncryptCookies::class,
+            //AddQueuedCookiesToResponse::class,
+            //ShareErrorsFromSession::class,
+            //AuthenticateWithBasicAuth::class,
+            //Authenticate::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/api.php
+++ routes/api.php
@@ -1,2 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
+// In other to make the auth middleware to work on the api file, we need to keep the login
+// route in the auth file which uses the web guard.
+Route::middleware('auth.basic')->group(function () {
+    //
+});
